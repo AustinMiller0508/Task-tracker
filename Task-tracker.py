33e428d85aa60ec54f60fb09f7c61f3c01f39c42
AustinMiller0508 <130001458+AustinMiller0508@users.noreply.github.com>
2023-07-27 15:48:09 -0600

    Added Functionality for creating a new task

--- Task-tracker.py
+++ Task-tracker.py
@@ -11,30 +11,57 @@
 #   Drop
 
 import time
+noVariations = ["no","none","n/a","nah","nope"]
+tasks = []
 
 
 class Task:
     def __init__(self):
-        self.dueDate = 0
+        self.dueDate = time.time()
         self.status = "To Do"
         self.name = ""
         self.description = ""
         
-def main_menu():
+def mainMenu():
     programExit = False
     
     while(programExit == False):
-        print("          Menu          \n------------------------\n1)    Today at a glance\n2)    Week at a glance\n3)    New task\n4)    Update task\n5)    View task\n6)    Last 10 Accomplishments\n7)    Quit\n-----------------------\n\nPlease select the desired action:")
+        print("          Menu          \n------------------------\n1)    Today at a glance\n2)    Week at a glance\n3)    New task\n4)    Update task\n5)    View tasks\n6)    Last 10 Accomplishments\n7)    Quit\n-----------------------\n\nPlease select the desired action:")
         selected = input()
-        if(selected == "1"):
-            print("Nioce")
+        if(selected == "3"):                        #if menu option 3 is chosen run the create task function
+            createTask()
         if(selected == "7"):
             print("See you again soon!")
             programExit = True
 
+def createTask():
+    validDate = False
+    
+    curTask = Task()                                #create new task and add to list of tasks
+    tasks.append(curTask)
+    print("\nWhat would you like this task to be named?\n")
+    name = input()                                  #gather task name
+    print("\nPlease add a description for the task.\n")
+    description = input()                           #gather task description
+    print("\nWhen is this task due? Please use the mm/dd/yyyy format")
+    while (validDate == False):
+        date = input()
+        try:
+            realDate = time.strptime(date, "%m/%d/%Y")  #convert user input to time for later comparison
+            validDate = True
+        except:
+            print("\nThe date you entered is invalid, please try again...\n")
+
+    curTask.name = name                             #Set task values based on user input
+    curTask.description = description
+    curTask.dueDate = realDate
+
+    print("\nTask created successfully!\n")
+    return
+
 def main():
     
-    main_menu()
+    mainMenu()
 
     return 0
 
